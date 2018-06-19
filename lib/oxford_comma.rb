c8714a58d3758edd10563d53ec4d3a5a08f9012e
Jack Yeung <jyeung.se@gmail.com>
2018-06-19 01:00:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    array.join(" and ")
+  elsif array.size >= 3
+    last_element = array.pop
+    array.join(", ") << ", and #{last_element}"
+  else 
+  array.join(", ")
+  end
 end
